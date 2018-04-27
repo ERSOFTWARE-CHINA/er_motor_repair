def4ea5c79c058663c0ccc2f7528e8190da280c7
trimono <threemonoliths@gmail.com>
2018-04-27 22:57:39 +0800

    修改维修信息路由名称

--- motor_repair_backend/lib/motor_repair_backend_web/router.ex
+++ motor_repair_backend/lib/motor_repair_backend_web/router.ex
@@ -39,7 +39,7 @@ defmodule MotorRepairBackendWeb.Router do
     resources "/users", UserController, except: [:new, :edit]
     resources "/projects", ProjectController, except: [:new, :edit]
     resources "/roles", RoleController, except: [:new, :edit]
-    resources "/repairinfo", RepairInfoController, except: [:new, :edit]
+    resources "/repair_info", RepairInfoController, except: [:new, :edit]
 
     resources "/carMessage", CarMessageController, except: [:new, :edit]
   end
