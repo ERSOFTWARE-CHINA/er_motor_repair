61e27b8ba55858b488b76c2a60c84609b6bae34d
tms <threemonoliths@gmail.com>
2018-05-16 16:22:14 +0800

    heatbeat

--- motor_repair_frontend/src/app/routes/auth.guard.ts
+++ motor_repair_frontend/src/app/routes/auth.guard.ts
@@ -10,12 +10,12 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router,public aclSrv: ACLService,private menuSrv: MenuService) { }
  
     canActivate() {
-        // if (localStorage.getItem('currentToken')) {
-        //     return true;
-        // }
-        // this.router.navigate(['/login']);
-        // return false;
-        return true
+        if (localStorage.getItem('currentToken')) {
+            return true;
+        }
+        this.router.navigate(['/passport/login']);
+        return false;
+        // return true
     }
 
 }
